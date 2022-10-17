2d3de28ce02882f036bf797a62628b46b0c9ecb4
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-10-17 09:03:27 +0500

    Add files via upload

---
+++ дз4-2.cpp
@@ -0,0 +1,33 @@
+﻿/* TheTri7 Найтите все числа из промежутка[a, b], которые при возведении
+в квадрат дают пальндром.*/
+#include <iostream>
+
+using namespace std;
+
+int pal(int f)
+{
+	int x = f;
+	int s = 0;
+	while (x)
+	{
+		s = s * 10 + x % 10;
+		x /= 10;
+	}
+	return f == s;
+}
+
+int main()
+{
+	int a, b, k;
+	cin >> a >> b;
+	for (a; a <= b; a++)
+	{
+		k = 0;
+		if (pal(a * a))
+			
+			cout << a << endl;
+		    k += 1;
+	}
+}
+// a=48, b=101 -> 101
+// a=300, b=1000 -> 307 836
